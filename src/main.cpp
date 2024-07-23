389994f90847c061976d488246ba0859fce130b9
Natalia Raźny <franek.razny@gmail.com>
2024-07-23 23:04:03 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <LCD_I2C.h>
+
+LCD_I2C lcd(0x27, 20, 4); // Default address of most PCF8574 modules, change according
+
+void setup()
+{
+    lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
+                 // this stop the library(LCD_I2C) from calling Wire.begin()
+    lcd.backlight();
+}
+
+void loop()
+{
+    lcd.print("     Hello"); // You can make spaces using well... spaces
+    lcd.setCursor(5, 1); // Or setting the cursor in the desired position.
+    lcd.print("World!");
+    delay(500);
+
+    // Flashing the backlight
+    for (int i = 0; i < 5; ++i)
+    {
+        lcd.backlight();
+        delay(50);
+        lcd.noBacklight();
+        delay(50);
+    }
+
+    lcd.backlight();
+    lcd.clear();
+    delay(500);
+}
